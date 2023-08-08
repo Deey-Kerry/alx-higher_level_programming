0b676ba8856e9b57ac287a93e11d7c9d61b637b4
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-08 12:23:58 -0700

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""A program function that prints reverse alphabet"""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
