b733a010af4f1e7606eec5611a6f419d9b131a9c
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-10 13:19:08 -0700

    2-args.py update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """A program function that prints the number and list arguements"""
     import sys
-    
     arg = sys.argv
-    size = len(arg) - 1
-    
-    if size == 0:
-        print("0 arguements.")
-    elif size == 1:
-        print("1 arguement:")
+    count = len(arg) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguements:".format(size))
-    for i in range(size):
-            print("{}: {}".format(i + 1, arg[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, arg[i + 1]))
