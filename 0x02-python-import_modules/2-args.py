698d637cd619c611bb6d75f15670b2b3eac83034
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-10 13:13:31 -0700

    2-args.py update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     import sys
     
     arg = sys.argv
-    count = len(arg) - 1
+    size = len(arg) - 1
     
-    if count == 0:
+    if size == 0:
         print("0 arguements.")
-    elif count == 1:
+    elif size == 1:
         print("1 arguement:")
     else:
-        print("{} arguements:".format(count))
-    for i in range(count):
+        print("{} arguements:".format(size))
+    for i in range(size):
             print("{}: {}".format(i + 1, arg[i + 1]))
