7e9f7687ebdfd19614775270cbd412f65cd7cd7c
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-10 14:37:00 -0700

    100-my_calculator.py update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """A program that imports all functions from the file calculator_1.py and handles basic operations."""
-    from sys import argv
+    """A program that imports all functions from the file calculator"""
     from calculator_1 import add, sub, mul, div
-    argc = len(argv)
-    if argc != 4:
-        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
-        exit(1)
-    ops = {'+': add, '-': sub, '*': mul, '/': div}
-    if argv[2] in ops:
-        num1 = int(argv[1])
-        num2 = int(argv[3])
-        op = ops[argv[2]]
-        result = op(num1, num2)
-        print('{:d} {:s} {:d} =  {:d}'.format(num1, argv[2], num2, result))
-    else:
+    from sys
+
+    arg = sys.argv 
+    if len(arg) -1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+        
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if arg[2] not in list(ops.keys()):
         print('Unknown operator. Available operators: +, -, *, /')
-        exit(1)
-    exit(0)
+        sys.exit(1)
+
+        num1 = int(arg[1])
+        num2 = int(argv[3])
+        print("{} {} {} =  {}".format(num1, arg[2], num2, ops[arg[2]](a, b)))
