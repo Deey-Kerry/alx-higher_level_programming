fbdd429dbfd81b8071875961f67671da610f6118
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-10 15:04:44 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    k = len(argv) - 1
-    if k == 0:
-        print("{:d}".format(k))
-        return
-    else:
-        i = 0
-        add = 0
-        while i <= k:
-            add += 1
-        print("{:d}".format(add))
-
 if __name__ == "__main__":
+    """Program that handles infinite numbers"""
     import sys
-    add_arg(sys.argv)
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
