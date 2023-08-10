b53a201b20a274dd75d1a1dba851357c58d7bab4
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-10 13:54:31 -0700

    102-magic_calculation.py update

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     """Function that does exactly the same as below Python bytecode"""
     from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
