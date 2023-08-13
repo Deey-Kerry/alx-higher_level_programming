41baa935bf715197359f26c7ac2021273c5cdc61
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-13 14:48:37 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     """a function that removes all characters c  from a string."""
     new_string = ""
-    for i in my_string:
-        if (i != 'c') and (i != 'C'):
-            new_string += i
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
         return new_string
 
