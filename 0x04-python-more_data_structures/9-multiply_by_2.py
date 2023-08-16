bb73724b88861dd7abae480060c686339558afb1
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-16 06:29:41 -0700

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """a function that returns a new dictionary with all values"""
+    return {key: value * 2 for key, value in a_dictionary.items()}
