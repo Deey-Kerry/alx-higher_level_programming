bab30b223c197248f5fdd826a202228845e13618
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-28 13:34:07 -0700

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
-    """a function that prints the first x elements of a list and only integers."""
-    sum_total = 0
-    for i in range(0, x):
+    """a function that prints the first x elements of a list and only integers"""
+    total = 0
+    for a in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            sum_total += 1
+            print("{:d}".format(my_list[a]), end="")
+            total += 1
         except (ValueError, TypeError):
             continue
-        print("")
-        return (sum_total)
+    print("")
+    return (total)
