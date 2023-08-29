8a91a551a3ba50500313b0876ad199a571b43977
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-08-29 12:54:36 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square that defines a Square"""
+
+
+class Square:
+    """Square based on 0-square.py"""
+    def __init__(self, size):
+        """Square initialization"""
+        self.__size = size
