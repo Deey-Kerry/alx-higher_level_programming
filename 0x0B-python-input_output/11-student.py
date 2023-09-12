2312bea58950886c36504571fe66b226fc107478
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-09-12 15:06:28 -0700

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+a class Student that defines a student
+"""
+
+
+class Student():
+    """
+    Public instance attributes:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance:"""
+        for k, v in json.items():
+            setattr(self, k, v)
