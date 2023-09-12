e26cf440b2f2ad6b7222e72ad5208d82866bc9c7
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-09-12 16:40:22 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-script that reads stdin line by line and computes metrics
-"""
+"""script that reads stdin line by line and computes metrics"""
 
 
 def print_stats(size, status_codes):
