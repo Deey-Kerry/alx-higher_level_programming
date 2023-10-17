f9e890d7445ddebfd7a8ae96d944b7587cc9cf78
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-10-17 08:10:45 -0700

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
 -- lists all records of the table second_table of the database
---Results should display both the score and the name (in this order)
-
 SELECT score, name FROM second_table ORDER BY score DESC;
