35158aa3cdebb91cea6cc00efb75658c4f7c2110
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-13 14:35:30 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,9 @@
 const lines = [
   'C is fun',
   'Python is cool',
-  'Javascript is amazing'
+  'JavaScript is amazing'
 ];
+  
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
 }
