bab31187021419aa5c1c41040d1a0724e0641921
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-13 15:10:10 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-  console.log(process.argv[2]);
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-  console.log('No arguement');
+  console.log(process.argv[2]);
 }
