af59e5968af6cf60c9b923ccc43c2eca5f6d58bc
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-18 12:06:22 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,11 +14,10 @@ Access to the database for the states
 if __name__ == '__main__':
     db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
-
     db_cursor = db_connect.cursor()
 
     db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+        "SELECT * FROM states WHERE name LIKE BINARY '%N' \
                 ORDER BY states.id ASC")
 
     rows_selected = db_cursor.fetchall()
