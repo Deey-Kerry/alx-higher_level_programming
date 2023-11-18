a6ca21cbb62b140a74194826a286a17655da97a1
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-18 12:42:20 -0800

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,16 +11,15 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database of the states
+    Access to the database of the
+    states
     """
     conn = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = conn.cursor()
-
     db_cursor.execute(
         "SELECT * FROM states WHERE name LIKE BINARY \
                 %(name)s ORDER BY id ASC", {'name': argv[4]})
     rows_selected = db_cursor.fetchall()
-
     for row in rows_selected:
         print(row)
