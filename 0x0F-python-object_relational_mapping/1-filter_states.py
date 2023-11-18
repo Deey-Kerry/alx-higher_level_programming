a20c903851d9a4f9d70f58c00d9e3b48ca026dae
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-18 12:15:24 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-A script that lists all states with a name starting with N 
-from the database hbtn_0e_0_usa
+A script that lists all states with a name starting with the
+letter N from the database hbtn_0e_0_usa
 """
 
 import MySQLdb as db
 from sys import argv
 
 """
-Access to the database for the states
+Access to the database of the states
 """
 
 if __name__ == '__main__':
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     db_cursor = db_connect.cursor()
 
     db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '%N' \
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                 ORDER BY states.id ASC")
 
     rows_selected = db_cursor.fetchall()
