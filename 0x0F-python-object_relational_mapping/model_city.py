1bd96bbce6230993934bb5984f6fb44630d7b84d
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-19 01:57:08 -0800

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script defines a City class and a Base class
+"""
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """
+    City class
+    The table name of the class
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script defines a State class and a Base class
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class
+    The table name of the class
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
