b5eeccfba33109174c55360eab1138a941a04ece
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-30 05:34:42 -0800

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/bin/bash
 #sends a request to that URL, and displays the size of the response
-curl -sI "$1" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r'
+curl -sI "$1" | grep  "Content-Length" | cut -d' ' -f2
